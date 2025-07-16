1b3f6bca61469c0dbfe511ca70f49b1b3757ff68
vidhusan12 <vidhusan709@gmail.com>
2025-07-17 04:36:25 +1200

    Added edit Button and its logic

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => {
     //this code here... fires when the app.js loads
     db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      setTodos(snapshot.docs.map(doc => doc.data().todo))
+      setTodos(snapshot.docs.map(doc => ({ id: doc.id, todo: doc.data().todo })))
     })
   }, []);
 
@@ -53,7 +53,7 @@ function App() {
 
       <ul>
         {todos.map(todo => (
-          <Todo text={todo} />
+          <Todo todo={todo} />
         ))}
       </ul>
 
--- todo_app/src/components/Todo.js
+++ todo_app/src/components/Todo.js
@@ -1,16 +1,61 @@
-import { List, ListItem, ListItemAvatar, ListItemText } from "@mui/material"
-import React from "react"
+import { Box, Button, Input, List, ListItem, ListItemAvatar, ListItemText, Modal } from "@mui/material"
+import React, { useState } from "react"
 import './Todo.css'
+import db from "../firebase"
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
 
 export default function Todo(props) {
 
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState('');
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const updateTodo = () => {
+    //update tge todo with the new input 
+    db.collection('todos').doc(props.todo.id).set({
+      todo: input
+    }, { merge: true })
+    setOpen(false);
+  }
 
   return (
-    <List className="todo_list">
-      <ListItem>
-        <ListItemAvatar></ListItemAvatar>
-        <ListItemText primary={props.text} secondary="Dummy deadline"/>
-      </ListItem>
-    </List>
+    <>
+      <Modal
+        open={open}
+        onClose={e => setOpen(false)}
+      >
+        <Box sx={{ ...style, width: 200 }}>
+          <h1>Edit..</h1>
+          <Input placeholder={props.todo.todo} value={input} onChange={event => setInput(event.target.value)} />
+          <Button onClick={updateTodo}>Update Todo</Button>
+        </Box>
+
+      </Modal>
+      <List className="todo_list">
+        <ListItem>
+          <ListItemAvatar></ListItemAvatar>
+          <ListItemText primary={props.todo.todo} secondary="Dummy deadline" />
+        </ListItem>
+        <button onClick={e => setOpen(true)}>Edit</button>
+        <Button onClick={event => db.collection('todos').doc(props.todo.id).delete()}>Delete Me</Button>
+      </List>
+    </>
   )
 }
